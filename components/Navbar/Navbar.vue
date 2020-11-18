b6282690a1f2f32b84ad73d7af75bfae8867e6b5
Kyrobane <koshnawaza@gmail.com>
2020-11-19 01:34:07 +0700

    more change

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -5,7 +5,7 @@
         <slot v-bind="slotData"></slot>
         <navbar-toggle-button
           :toggled="showMenu"
-          @click="toggle"
+          @click.native.prevent="toggle"
         ></navbar-toggle-button>
       </div>
       <div
